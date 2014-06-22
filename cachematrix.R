f4edd8afc26df81f2910d197045a41010d8c52e5
klehma <klehma13@gmail.com>
2014-06-22 11:46:48 -0600

    Functions added for homework
    the makeCacheMatrix and cacheSolve functions have been created and
    added to the file cache matrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allow the caching of the inverse of a matrix
+## to enable faster retrieval
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special matrix object that is able
+## to cache its own inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
+    inv <- NULL
+    set <- function(y) {
+      x <<- y
+      inv <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(solve) inv <<- solve
+    getInverse <- function() inv
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
+  }
+  
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the inverse of the argument matrix, using 
+## the cached value if it has already been solved
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+  inv <- x$getInverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setInverse(inv)
+  inv
+  
 }
